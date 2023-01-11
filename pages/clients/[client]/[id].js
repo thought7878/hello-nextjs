745b5de0a02fc3cd2f706517963af793ac74e31f
Calm Andy <thought7878@qq.com>
2023-01-11 15:12:35 +0800

    Nested Dynamic Routes & Paths

---
+++ pages/clients/[client]/[id].js
@@ -0,0 +1,9 @@
+import { useRouter } from "next/router";
+
+export default function ClientDetailPage() {
+  const router = useRouter();
+  console.log("router.pathname:", router.pathname);
+  console.log("router.query:", router.query);
+
+  return <div>Client Detail Page</div>;
+}
---
+++ pages/clients/index.js
@@ -0,0 +1,9 @@
+import { useRouter } from "next/router";
+export default function ClientListPage() {
+  const router = useRouter();
+
+  console.log("router.pathname:", router.pathname);
+  console.log("router.query:", router.query);
+
+  return <div>Client List Page</div>;
+}
