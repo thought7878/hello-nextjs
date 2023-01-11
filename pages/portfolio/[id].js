7ee218b2f776f36d4692efea7536f2a2f1abb4ed
Calm Andy <thought7878@qq.com>
2023-01-11 14:38:56 +0800

    router.pathname/router.query

--- pages/portfolio/[id].js
+++ pages/portfolio/[id].js
@@ -1,3 +1,10 @@
+import { useRouter } from "next/router";
+
 export default function PortfolioProjectPage() {
+  const router = useRouter();
+
+  console.log("router.pathname:", router.pathname);
+  console.log("router.query:", router.query);
+
   return <div>Portfolio Project Page</div>;
 }
