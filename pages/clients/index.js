70bd364b1296219712ee9dbc14ad09f051fbb14b
Calm Andy <thought7878@qq.com>
2023-01-11 16:54:48 +0800

    next/Link basic

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,9 +1,23 @@
+import Link from "next/link";
 import { useRouter } from "next/router";
+
 export default function ClientListPage() {
   const router = useRouter();
 
   console.log("router.pathname:", router.pathname);
   console.log("router.query:", router.query);
 
-  return <div>Client List Page</div>;
+  return (
+    <div>
+      <h1>Client List Page</h1>
+      <ul>
+        <li>
+          <Link href={`/clients/client1`}>client1</Link>
+        </li>
+        <li>
+          <Link href={`/clients/client2`}>client2</Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import Image from "next/image";
+import Link from "next/link";
 import { Inter } from "@next/font/google";
 import styles from "../styles/Home.module.css";
 
@@ -8,14 +8,21 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <>
-      <Head>
+      {/* <Head>
         <title>Hello nextJS App</title>
         <meta name="description" content="Hello nextJS" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
-      </Head>
+      </Head> */}
       <main className={styles.main}>
         <div className={styles.center}>Hello nextJS</div>
+        <ul>
+          <li>
+            <Link href={"/portfolio"}>link to portfolio</Link>
+          </li>
+          <li></li>
+          <li></li>
+        </ul>
       </main>
     </>
   );
